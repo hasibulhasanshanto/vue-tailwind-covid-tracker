be1171b1b0b3766576f8f4778f8ce518b6013784
kajalshanto <hasibulhasan.kajal@gmail.com>
2022-04-16 14:50:30 +0600

    Complete the covid tracker

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="mt-10 mb-4">
+      <label>Country Tracker</label>
+      <select
+        @change="countryChange()"
+        v-model="selected"
+        class="form-select block w-full p-3 rounded border-2"
+      >
+        <option value="" selected disabled>Please choose a country</option>
+        <option
+          v-for="country in countries"
+          :value="country.ID"
+          :key="country.ID"
+        >
+          {{ country.Country }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: ["countries"],
+  data() {
+    return {
+      selected: "",
+    };
+  },
+  methods: {
+    countryChange() {
+      const country = this.countries.find((item) => item.ID == this.selected);
+
+      this.$emit("get-country", country);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,16 @@
   <main v-if="!loadingLoader">
     <Datatitle :title="title" :todayDate="todayDate" />
     <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <div class="mb-20 mt-2" v-if="stats.Country">
+      <button
+        @click="clearCountryData()"
+        class="bg-purple-700 text-white rounded p-3 focus:outline-none hover:bg-purple-800"
+      >
+        Clear Country
+      </button>
+    </div>
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
@@ -13,12 +23,14 @@
 import axios from "axios";
 import Datatitle from "@/components/DataTitle.vue";
 import DataBoxes from "@/components/DataBoxes.vue";
+import CountrySelect from "@/components/CountrySelect.vue";
 
 export default {
   name: "Home",
   components: {
     Datatitle,
     DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -36,7 +48,6 @@ export default {
       const res = await axios
         .get("https://api.covid19api.com/summary")
         .then((res) => {
-          console.log(res.data);
           self.todayDate = res.data.Date;
           self.stats = res.data.Global;
           self.countries = res.data.Countries;
@@ -46,10 +57,19 @@ export default {
         })
         .finally(() => (self.loadingLoader = false));
     },
+    getCountryData(country) {
+      (this.stats = country), (this.title = country.Country);
+    },
+    async clearCountryData() {
+      let self = this;
+      self.loadingLoader = true;
+      await self.fetchCovidData();
+      self.title = "Global";
+      self.loadingLoader = false;
+    },
   },
   async created() {
     let self = this;
-    console.log("Created");
     await self.fetchCovidData();
   },
 };
